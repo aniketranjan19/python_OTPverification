de82a2161048014a76fc3beddcc998f234d1b1ff
Aniket Ranjan <127397091+aniketranjan19@users.noreply.github.com>
2023-03-14 19:18:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ import smtplib
 # generating random 6-digit otp
 digits = "0123456789"
 OTP = ""
-for i in range(6):
+for i in range(0, 6):
     OTP = OTP + digits[math.floor(random.random()*10)]
-message = OTP + " is your OTP"
+message = OTP + " is your One Time Password for verification process."
 
 
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login("thecoderguy111@gmail.com", "qnqv vkit aijs zhsz")
+s.login("thecoderguy111@gmail.com", "qnqv vkit aijs zhsz")    # emailid and app password required for sending mail
 email = input("Enter your email: ")
-s.sendmail('&&&&&&&&&&&', email, message)
+s.sendmail('&&&&&&&&&&&', email, message)    # sending otp via email 
 
 
 # verifying otp
